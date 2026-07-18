087536146c8a3df7915d91aa4d8f8ffdcd2ca58a
itacode <9909272+itacode@users.noreply.github.com>
2026-07-18 14:13:33 +0900

    adjust eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,17 +1,13 @@
 import js from '@eslint/js';
-import globals from 'globals';
 import tseslint from 'typescript-eslint';
 import { defineConfig } from 'eslint/config';
 import eslintConfigPrettier from 'eslint-config-prettier';
 
-export default defineConfig([
-  {
-    files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
-    plugins: { js },
-    extends: ['js/recommended'],
-    languageOptions: { globals: { ...globals.browser, ...globals.node } },
-  },
-  { files: ['**/*.js'], languageOptions: { sourceType: 'commonjs' } },
-  tseslint.configs.recommended,
-  eslintConfigPrettier,
-]);
+export default defineConfig({
+  files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
+  extends: [
+    js.configs.recommended,
+    tseslint.configs.recommended,
+    eslintConfigPrettier,
+  ],
+});
